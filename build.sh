173a4e73585159d0382715f51fdd22295522bd45
Alistair Miles <alimanfoo@googlemail.com>
2017-02-15 10:29:44 +0000

    cache build dir

--- build.sh
+++ build.sh
@@ -9,13 +9,9 @@ export PATH=./dependencies/miniconda/bin:$PATH
 # add texlive to the path
 export PATH=./dependencies/texlive/2016/bin/x86_64-linux:$PATH
 
+# ensure build directory exists
+mkdir -pv build
+
 # build
 snakemake all
 
-# test latex
-#tex --version
-#pdflatex main.tex
-
-# test miniconda
-#source activate scipy
-#python -c 'import sys; print(sys.version_info); import numpy; print(numpy.__version__)'
